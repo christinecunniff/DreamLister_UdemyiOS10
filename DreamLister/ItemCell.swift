67e9749a3cd315ef34c2f06c591894c7768abb48
Christine Cunniff <christinecunniff@gmail.com>
2016-12-13 10:14:35 -0500

    Connected ItemCell outlets.

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
